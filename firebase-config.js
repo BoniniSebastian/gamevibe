5438febef3ed1b42cd159338be4d221e0f4acb37
BoniniSebastian <sebastian.bonini@gmail.com>
2026-03-27 14:55:34 +0100

    Add Firebase configuration and Firestore initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,15 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "DIN_API_KEY",
+  authDomain: "DIN_DOMAIN",
+  projectId: "gamevibe-88e75",
+  storageBucket: "DIN_BUCKET",
+  messagingSenderId: "DIN_ID",
+  appId: "DIN_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
